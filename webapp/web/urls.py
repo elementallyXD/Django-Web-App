ba12119998953821fb1140aeffd384f5e1a86727
Bidiak Mykhailo <sgame666@gmail.com>
2021-03-24 13:32:48 +0200

    Added about page with logo and app description

--- webapp/web/urls.py
+++ webapp/web/urls.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
+from django.conf.urls import url
 from .api import TodoViewSet, CategoryViewSet
-from .views import TodoViewSet1, UserViewSet
-
+from .views import TodoViewSet1, UserViewSet, dashboard
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 router = routers.DefaultRouter()
 router.register('api/web', TodoViewSet, 'web')
@@ -9,4 +10,9 @@ router.register('todo', TodoViewSet1, 'web')
 router.register('users', UserViewSet)
 router.register('api/categories', CategoryViewSet, 'categories')
 
-urlpatterns = router.urls
+urlpatterns = [
+    url(r"^about/", dashboard, name="about"),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += router.urls
--- webapp/web/views.py
+++ webapp/web/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
 from .models import Todo
 from .serializers import TodoSerializer, UserSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from django.contrib.auth.models import User
+from django.shortcuts import render
 # Create your views here.
+
 
 class TodoViewSet1(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     authentication_classes = (TokenAuthentication,)
 
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
+def dashboard(request):
+    return render(request, "about/about.html")
